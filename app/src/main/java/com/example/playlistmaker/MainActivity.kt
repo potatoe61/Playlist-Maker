68143ee12aad44e942700f9002fb2307872ee166
Dimasique <dima-ratov@mail.ru>
2024-10-17 14:48:19 +0300

    Спринт 8. Добавил лишнюю строку чтобы была разница для пул реквеста

---
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val search = findViewById<Button>(R.id.search)
+
+        search.setOnClickListener {
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
+        }
+        val mediateka = findViewById<Button>(R.id.mediateka)
+
+        mediateka.setOnClickListener {
+            val displayIntent = Intent(this, MediatekaActivity::class.java)
+            startActivity(displayIntent)
+        }
+        val settings = findViewById<Button>(R.id.settings)
+
+        settings.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
+        }
+}
+}
