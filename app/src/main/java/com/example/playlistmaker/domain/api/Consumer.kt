afbf8a1e81bb5941880240624fb73e1bc19ccd12
potatoe61 <dima-ratov@mail.ru>
2025-03-24 17:10:18 +0300

    Sprint 16 try 1

--- app/src/main/java/com/example/playlistmaker/domain/api/Consumer.kt
+++
@@ -1,5 +0,0 @@
-package com.example.playlistmaker.domain.api
-
-interface Consumer<T> {
-    fun consume(data: ConsumerData<T>)
-}
--- app/src/main/java/com/example/playlistmaker/domain/repositories/TrackRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.playlistmaker.domain.repositories
-
-import com.example.playlistmaker.domain.api.Resource
-import com.example.playlistmaker.domain.models.Track
-
-interface TrackRepository {
-    fun searchTrack(expression : String): Resource<List<Track>>
-}
