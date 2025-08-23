0f8034b706cf8ec52db4e4ff5109cc15d88d0766
potatoe61 <dima-ratov@mail.ru>
2025-08-23 19:52:04 +0300

    Sprint 20 try1

--- app/src/main/java/com/example/playlistmaker/search/domain/repositories/TrackRepository.kt
+++ app/src/main/java/com/example/playlistmaker/search/domain/repositories/TrackRepository.kt
@@ -2,9 +2,10 @@ package com.example.playlistmaker.search.domain.repositories
 
 import com.example.playlistmaker.search.domain.api.Resource
 import com.example.playlistmaker.search.domain.model.Track
+import kotlinx.coroutines.flow.Flow
 
 interface TrackRepository {
-    fun searchTracks(expression: String): Resource<List<Track>>
+    fun searchTracks(expression: String): Flow<Resource<List<Track>>>
 
     fun saveTrackToHistory(tracks: ArrayList<Track>)
     fun getHistoryTrack(): ArrayList<Track>
